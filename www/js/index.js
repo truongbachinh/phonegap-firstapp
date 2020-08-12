b7c99f2031f726381225fcf183407d40dc503374
truongbachinh <58037395+truongbachinh@users.noreply.github.com>
2020-08-12 09:45:03 +0700

    fix1

--- www/js/index.js
+++ www/js/index.js
@@ -36,12 +36,5 @@ var app = {
     app.receivedEvent('deviceready');
   },
   // Update DOM on a Received Event
-  receivedEvent: function (id) {
-    // var parentElement = document.getElementById(id);
-    // var listeningElement = parentElement.querySelector('.listening');
-    // var receivedElement = parentElement.querySelector('.received');
-    // listeningElement.setAttribute('style', 'display:none;');
-    // receivedElement.setAttribute('style', 'display:block;');
-    // console.log('Received Event: ' + id);
-  },
+  receivedEvent: function (id) {},
 };
